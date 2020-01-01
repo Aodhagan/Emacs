568eb97118579dba2a14a73f47c2f2f3da10ca98
Aodhagan <michael.m.malone@protonmail.com>
2020-01-01 12:53:35 +0000

    initial commit

---
+++ init.el
@@ -0,0 +1,25 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (zerodark-theme zenburn-theme yasnippet-snippets which-key use-package try tomatinho symon switch-window swiper spacemacs-theme spaceline smex redtick rainbow-delimiters popup-kill-ring pomidor pdf-tools org-bullets monokai-theme mark-multiple latex-preview-pane ido-vertical-mode hungry-delete highlight-parentheses git-commit f expand-region ess diminish dashboard cyberpunk-theme color-theme auto-complete ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
