82fc192f5a64fde9353ad375f3e84895018c76bc
Aodhagan <michael.m.malone@protonmail.com>
2020-02-03 10:18:12 +0100

    change pomidor typeface

--- init.el
+++ init.el
@@ -18,10 +18,12 @@
    (quote
     (zerodark-theme zenburn-theme yasnippet-snippets which-key use-package try tomatinho symon switch-window swiper spacemacs-theme spaceline smex redtick rainbow-delimiters popup-kill-ring pomidor pdf-tools org-bullets monokai-theme mark-multiple latex-preview-pane ido-vertical-mode hungry-delete highlight-parentheses git-commit f expand-region ess diminish dashboard cyberpunk-theme color-theme auto-complete ace-window)))
  '(pomidor-break-seconds 240)
- '(pomidor-seconds 900))
+ '(pomidor-seconds 900)
+ '(pomidor-sound-break-over ""))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(pomidor-time-face ((t (:height 3.0))))
+ '(pomidor-timer-face ((t (:height 3.0)))))
